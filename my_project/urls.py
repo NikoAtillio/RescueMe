57651a6b82c57f06cc673b6d63576d7624eea822
Nikos Norman <nikos_norman87@hotmail.com>
2024-12-19 17:23:29 +0000

    Cleaned html files

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,29 +1,16 @@
 """
 URL configuration for my_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from hello_world import views as index_views
 from django.conf import settings
 from django.conf.urls.static import static
+from hello_world import views as index_views
 
 urlpatterns = [
     # Admin URL
     path('admin/', admin.site.urls),
-    
+
     # Main pages
     path('', index_views.index, name='index'),
     path('who-are-we/', index_views.who_are_we, name='who_are_we'),
@@ -35,6 +22,6 @@ urlpatterns = [
     path('partners/', index_views.partners, name='partners'),
 ] 
 
-# Add this to serve static files during development
+# Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
