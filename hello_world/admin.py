cb4b1db368bc3b37475b0cbf4a82ca39daadd388
Nikos Norman <nikos_norman87@hotmail.com>
2025-01-05 17:28:36 +0000

    Fix Post model import issue and remove duplicate admin registration

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ('status', 'created_on')
-    search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}  # Auto-generates slug from title
-    list_editable = ['status']  # Allow status to be changed from list view
